aa7b4c063179b75a7414bc1129feb47e844e35f7
MarshallXXVI <Soonthornvech3@gmail.com>
2023-07-23 15:52:00 +0200

    update 23.7.23 15:51

--- PairWeise/solverMain.cpp
+++ PairWeise/solverMain.cpp
@@ -8,13 +8,14 @@
 int main(int argc, char **argv) {
   Checker c;
   // Parse the command-line arguments.
-  if (argc != 3) {
-    std::cout << "Usage: ./Main [file] [file]" << std::endl;
+  if (argc != 4) {
+    std::cout << "Usage: ./Main [file] [file] [file]" << std::endl;
     std::exit(1);
   }
 
   std::string optionFile = argv[1];
   std::string constraintsFile = argv[2];
+  std::string modelsFile = argv[3];
   c.readDataInput(optionFile, constraintsFile);
   if (!c.ifOptionAndConstraints()) {
     return c.errorCode_;
@@ -26,7 +27,7 @@ int main(int argc, char **argv) {
   s.getDataFromOption(p.getOptions());
   s.getDataFromConstraint(p.getConstraints());
   s.generateModells();
-  c.readDataModel("a.models");
+  c.readDataModel(modelsFile);
   if (!c.ifModel()) {
     return c.errorCode_;
   }
